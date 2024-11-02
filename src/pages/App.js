7e4f095d8782feed550c5683c2c06bfc49c0ede7
Fernando Laterza <fhlaterza@gmail.com>
2024-11-02 13:51:19 -0300

    input

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,14 @@
 import gitlogo from '../assets/github.webp'
+import Input from '../components/Input';
 import { Container } from './styles';
 
 function App() {
   return (
     <Container className="App">
       <img src = {gitlogo} width={72} height={72} alt="github logo" />
+      <Input />
     </Container>
-  );
+  )
 }
 
 export default App;
